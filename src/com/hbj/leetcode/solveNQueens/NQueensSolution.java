fd1176b45e27a18cbe74716b689765730cd742cd
1058232569 <1058232569@qq.com>
2020-04-28 17:31:32 +0800

    去掉一个类

--- src/com/hbj/leetcode/solveNQueens/NQueensSolution.java
+++ src/com/hbj/leetcode/solveNQueens/NQueensSolution.java
@@ -1,6 +1,7 @@
 package com.hbj.leetcode.solveNQueens;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -11,25 +12,17 @@ public class NQueensSolution {
 
 
     public static List<List<String>> solveNQueens(int n) {
-
         List<List<String>> result = new ArrayList<>();
-        QueenResult queenResult = QueenResult.init(n);
-        begin(0,0, queenResult, n, result);
+        begin(0,0, new String[n][n], new ArrayList<>(), new ArrayList<>(), n, result);
         System.out.println(result);
         return result;
     }
 
 
 
-    public static void begin(int row, int col, QueenResult queenResult, int n, List<List<String>> queenResults){
+    public static void begin(int row, int col, String[][] array, List<Queen> queens, List<String> stringResult, int n, List<List<String>> queenResults){
 
-        String[][] array = queenResult.getArray();
-        List<Queen> queens = queenResult.getQueens();
-        List<String> stringResult = queenResult.getResult();
-
-        head:
         for (int i=row; i<n; i++) {
-
             StringBuilder s = new StringBuilder();
             lable:
             for (int j = 0; j < n; j++) {
@@ -56,12 +49,18 @@ public class NQueensSolution {
                         continue lable;
                     }
                 }
-                QueenResult noQueenResult = new QueenResult(queens, array, stringResult, n);
-                makeQueen(i, j, queenResult, n, queenResults);
-                makeNoQueen(i, j, noQueenResult,n, queenResults);
-                return;
-//                break head;
+                List<Queen> newQueens = new ArrayList<>();
+                newQueens.addAll(queens);
+                String[][] newArray =  new String[n][n];
+                for (int k=0;k<n;k++){
+                    newArray[k] = Arrays.copyOf(array[k], array[k].length);
+                }
+                List<String> newStrResult = new ArrayList<>();
+                newStrResult.addAll(stringResult);
 
+                makeQueen(i, j, array, queens, stringResult, n, queenResults);
+                makeNoQueen(i, j, newArray, newQueens, newStrResult, n, queenResults);
+                return;
             }
             stringResult.add(s.toString());
         }
@@ -71,23 +70,21 @@ public class NQueensSolution {
         }
 
     }
-    public static void makeQueen(int row, int col, QueenResult queenResult, int n, List<List<String>> queenResults){
+    public static void makeQueen(int row, int col, String[][] array, List<Queen> queens, List<String> stringResult, int n, List<List<String>> queenResults){
         Queen queen = new Queen(row, col);
-        queenResult.getQueens().add(queen);
-        String[][] array = queenResult.getArray();
+        queens.add(queen);
         array[row][col] = "Q";
-        begin(row, col, queenResult, n, queenResults);
+        begin(row, col, array,queens, stringResult , n, queenResults);
     }
 
-    public static void makeNoQueen(int row, int col, QueenResult queenResult, int n, List<List<String>> queenResults){
-        String[][] array = queenResult.getArray();
+    public static void makeNoQueen(int row, int col, String[][] array, List<Queen> queens, List<String> stringResult, int n, List<List<String>> queenResults){
         array[row][col] = ".";
-        begin(row, col, queenResult, n, queenResults);
+        begin(row, col, array,queens, stringResult , n, queenResults);
     }
 
     public static void main(String[] args) {
         long begin = System.currentTimeMillis();
-        solveNQueens(4);
+        solveNQueens(8);
         long end = System.currentTimeMillis();
         System.out.println(end - begin);
     }

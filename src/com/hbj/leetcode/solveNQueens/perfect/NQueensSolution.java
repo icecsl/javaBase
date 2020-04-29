4bed5d85316b5139e42a892ce530603b7f18f7d3
1058232569 <1058232569@qq.com>
2020-04-29 13:51:16 +0800

    最最完美

--- src/com/hbj/leetcode/solveNQueens/perfect/NQueensSolution.java
+++ src/com/hbj/leetcode/solveNQueens/perfect/NQueensSolution.java
@@ -1,7 +1,9 @@
 package com.hbj.leetcode.solveNQueens.perfect;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: huangbingjing
@@ -12,21 +14,22 @@ public class NQueensSolution {
 
     public static List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<>();
+        Map<Integer, String> resultMap = new HashMap<>();
+        for (int i=0; i<n; i++) {
+            resultMap.put(i, getStrResult(n, i));
+        }
         List<Queen> queens = new ArrayList<>(n);
-        begin(0,0, n, queens,result);
+        begin(0, n, queens,resultMap,result);
         System.out.println(result);
         return result;
     }
 
-    public static void begin(int row, int col, int n, List<Queen> queens,  List<List<String>> queenResult) {
+    public static void begin(int row, int n, List<Queen> queens,  Map<Integer, String> resultMap, List<List<String>> queenResult) {
         begin:
         for (int i=row; i<n; i++) {
             lable:
             for (int j = 0; j < n; j++) {
                 for (Queen queen: queens) {
-                    if (i == queen.getI()) {
-                        continue begin;
-                    }
                     if (j == queen.getJ()) {
                         continue lable;
                     }
@@ -38,14 +41,14 @@ public class NQueensSolution {
                 newQueens.addAll(queens);
                 Queen queenI = new Queen(i,j);
                 newQueens.add(queenI);
-                begin(i+1,0, n, newQueens, queenResult);
+                begin(i+1, n, newQueens, resultMap, queenResult);
             }
             return;
         }
 
         List<String> s = new ArrayList<>();
         for (Queen queen :queens) {
-            s.add(getStrResult(n, queen.getJ()));
+            s.add(resultMap.get(queen.getJ()));
         }
         queenResult.add(s);
 
